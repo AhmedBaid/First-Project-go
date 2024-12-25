0e85fd85276a53f6fb73103a669c0baeeefb0673
AhmedBaid <150438476+AhmedBaid@users.noreply.github.com>
2024-12-25 18:19:18 +0100

    Add files via upload

---
+++ go-reloaded/functions/Low.go
@@ -0,0 +1,11 @@
+package functions
+
+import (
+	"strings"
+)
+
+
+func ToLower(s string) string {
+	str := strings.ToLower(s)
+	return str
+}
---
+++ go-reloaded/functions/Up.go
@@ -0,0 +1,10 @@
+package functions
+
+import (
+	"strings"
+)
+
+func ToUpper(s string) string {
+	str := strings.ToUpper(s)
+	return str
+}
